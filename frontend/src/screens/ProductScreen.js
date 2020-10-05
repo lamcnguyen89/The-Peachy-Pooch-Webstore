fb1d8548a43660cde7d3a948b39c0b08b2c0dbb5
Lam Nguyen <lamcnguyen89@gmail.com>
2020-10-05 02:33:16 -0400

    Worked out some bugs. I'm honestly not sure what the hell this huy in the tutorial is doing. He's not really great at explaining things

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,12 +7,12 @@ function ProductScreen(props) {
 
   // console.log(props.match.params.id);
   // const product = data.products.find(x => x._id === props.match.params.id)
-  const productDetails = useSelector(state => state.productDetails);
-  const {product, loading, error} = productDetails
-;  const dispatch = useDispatch;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(detailsProduct());
+    dispatch(detailsProduct(props.match.params.id));
     return () => {
       //
     };
@@ -23,51 +23,57 @@ function ProductScreen(props) {
       {/* Page Content */}
       <main className="container p-2 my-4">
         <Link to="/">Back to Result</Link>
-        <div className="row">
-          {/* /.col-lg-3 */}
-          <div className="col-lg-9">
-            <div className="card mt-4">
-              <img
-                className="card-img-top img-fluid"
-                src={product.image}
-                alt=""
-              />
-              <div className="card-body">
-                <h3 className="card-title">{product.name}</h3>
-                <h4>{product.price}</h4>
-                <p className="card-text">
-                  {product.description}
-                </p>
-                <span className="text-warning">Stars {product.rating}</span>
-                
+        {loading? <div>Loading...</div>:
+        error? <div>{error}</div>:
+          (
+            <div className="row">
+            {/* /.col-lg-3 */}
+            <div className="col-lg-9">
+              <div className="card mt-4">
+                <img
+                  className="card-img-top img-fluid"
+                  src={product.image}
+                  alt=""
+                />
+                <div className="card-body">
+                  <h3 className="card-title">{product.name}</h3>
+                  <h4>{product.price}</h4>
+                  <p className="card-text">
+                    {product.description}
+                  </p>
+                  <span className="text-warning">Stars {product.rating}</span>
+                  
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3">
-              <div className="details-action m-2">
-                <ul>
-                  <li>
-                    Status: {product.status}
-                  </li>
-                  <li>
-                    Qty: <select>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                    </select>
-                  </li>
-                  <li>
-                    <button>Add to Cart</button>
-                  </li>
-                </ul>
+              <div className="col-lg-3">
+                <div className="details-action m-2">
+                  <ul>
+                    <li>
+                      Status: {product.status}
+                    </li>
+                    <li>
+                      Qty: <select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                      </select>
+                    </li>
+                    <li>
+                      <button>Add to Cart</button>
+                    </li>
+                  </ul>
+                </div>
               </div>
+              {/* /.card */}
+  
+              {/* /.card */}
             </div>
-            {/* /.card */}
-
-            {/* /.card */}
+            {/* /.col-lg-9 */}
           </div>
-          {/* /.col-lg-9 */}
-        </div>
+          )
+        }
+
       </main>
     </div>
   );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,17 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import {productListReducer, productDetailsReducer} from './reducers/productReducers';
+import thunk from 'redux-thunk';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer
 })
 
-const store= creatStore(reducer, initialState);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store= createStore(
+    reducer, 
+    initialState, 
+    composeEnhancer(applyMiddleware(thunk))
+);
+export default store;
